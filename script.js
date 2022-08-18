b475d6c707a26d46090da2df13307679e2d1fc32
Andrey Dmitriev <admit8490@gmail.com>
2022-08-18 14:27:56 +0200

    added: eventlisteners

--- script.js
+++ script.js
@@ -5,3 +5,29 @@ const closeBtn = document.getElementById('close-btn');
 
 // console.log(openBtn, modalContainer, closeBtn);
 
+//functions
+
+const showModal = () => {
+  modalContainer.style.display = "block";
+}
+
+const hideModalbyBtn = () => {
+  modalContainer.style.display = "none";
+}
+
+const hideModalClickAnywhere = (e) => {
+  if (e.target === modalContainer) {
+    modalContainer.style.display = "none";
+  }    
+}
+
+// event listeners
+
+openBtn.addEventListener('click', showModal);
+
+closeBtn.addEventListener('click', hideModalbyBtn);
+
+// to hide modal by click on any point of viewport
+window.addEventListener('click', hideModalClickAnywhere);
+
+//TODO some testing with logging e.target
