d50b5cbe4e6da19483b62806afbdd52153b0fc3b
Andrey Dmitriev <admit8490@gmail.com>
2022-08-18 14:05:25 +0200

    added js constants

---
+++ script.js
@@ -0,0 +1,7 @@
+// constants
+const openBtn = document.getElementById('open-btn');
+const modalContainer = document.getElementById('modal-container');
+const closeBtn = document.getElementById('close-btn');
+
+// console.log(openBtn, modalContainer, closeBtn);
+
